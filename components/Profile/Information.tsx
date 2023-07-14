417f720d57a103d5db7cfa346a8131ea9f21d354
Diyar Karimzadeh <diyarkarimzadeh@Diyars-MacBook-Air.local>
2023-07-14 16:14:09 +0330

    fix information component problem

--- components/Profile/Information.tsx
+++ components/Profile/Information.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Information = () => {
   return (
-    <div className='mt-6'>
-        <div>
-            <p>Just passin' by.</p>
-        </div>
-        <div>
-            <p className='text-gray-600'>
-                26 followers
-            </p>
-        </div>
+    <div className="mt-6">
+      <div>
+        <p>Frontend Developer</p>
+      </div>
+      <div>
+        <p className="text-gray-600">26 followers</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Information
+export default Information;
