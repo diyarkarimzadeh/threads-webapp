c30ae8a8f98a474f6a814027fb846c17251b8533
Diyar Karimzadeh <diyarkarimzadeh@Diyars-MacBook-Air.local>
2023-07-15 00:57:04 +0330

    fix bugs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,26 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react'
-import BottomNavigation, { useStore } from '@/components/Navigation/BottomNavigation'
+import React, { useEffect } from "react";
+import BottomNavigation, {
+  useStore,
+} from "@/components/Navigation/BottomNavigation";
 
 const Search = () => {
-    const { setCurrentPage } = useStore();
+  const { setCurrentPage } = useStore();
 
-    useEffect(() => {
-        setCurrentPage(2);
-    }, [])
+  useEffect(() => {
+    setCurrentPage(2);
+  }, []);
   return (
     <div>
-        <p>Search</p>
-        <BottomNavigation/>
+      <div className="p-6">
+        <header className="flex flex-row items-center justify-start text-2xl font-semibold">
+          Search
+        </header>
+      </div>
+      <BottomNavigation />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- components/Profile/HeaderIcons.tsx
+++ components/Profile/HeaderIcons.tsx
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Lock, Instagram, AlignRight } from 'lucide-react'
+import React from "react";
+import { Lock, Instagram, AlignRight } from "lucide-react";
 
 const HeaderIcons = () => {
   return (
-    <div className='flex flex-row items-center justify-between'>
-        <div>
-          <Lock size={24}/>
-        </div>
-        <div className='flex flex-row gap-6'>
-          <Instagram size={24}/>
-          <AlignRight size={24}/>
-        </div>
+    <div className="flex flex-row items-center justify-between">
+      <div>
+        <Lock size={24} />
       </div>
-  )
-}
+      <div className="flex flex-row gap-6">
+        <Instagram size={24} />
+        <AlignRight size={24} />
+      </div>
+    </div>
+  );
+};
 
-export default HeaderIcons
+export default HeaderIcons;
