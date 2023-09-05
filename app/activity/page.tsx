c38f5b7f1be53e90bf5d949fcfec836ca915e263
Diyar Karimzadeh <diyar1379@gmail.com>
2023-09-06 01:11:53 +0330

    add some new features

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 
-import React from 'react'
-import BottomNavigation, { useStore } from '@/components/Navigation/BottomNavigation'
-import { useEffect } from 'react';
+import React from "react";
+import BottomNavigation, {
+  useStore,
+} from "@/components/Navigation/BottomNavigation";
+import { useEffect } from "react";
 
 const Activity = () => {
-    const { setCurrentPage } = useStore();
+  const { setCurrentPage } = useStore();
 
-    useEffect(() => {
-        setCurrentPage(4);
-    }, [])
+  useEffect(() => {
+    setCurrentPage(4);
+  }, []);
   return (
     <div>
-        <p>Activity</p>
-        <BottomNavigation/>
+      <p>Activity</p>
+      <BottomNavigation />
     </div>
-  )
-}
+  );
+};
 
-export default Activity
+export default Activity;
--- data/threads.ts
+++ data/threads.ts
@@ -75,3 +75,69 @@ export const threads: Thread[] = [
     createdAt: "3h",
   },
 ];
+
+export const mainThreads: Thread[] = [
+  {
+    id: 1,
+    username: "selenagomez",
+    userImage:
+      "https://www.fakepersongenerator.com/Face/female/female20161025235219666.jpg",
+    threadText: "Hey threads people, how you doing? 😍",
+    likes: 8546,
+    replies: 455,
+    userWhoReplies:
+      "https://www.fakepersongenerator.com/Face/male/male20161086287283478.jpg",
+    createdAt: "45m",
+  },
+  {
+    id: 2,
+    username: "petermckinnnon",
+    userImage:
+      "https://www.fakepersongenerator.com/Face/female/female1023072426972.jpg",
+    threadText: "Anybody in Toronto today? we have a photoshoot!",
+    likes: 980,
+    replies: 134,
+    userWhoReplies:
+      "https://www.fakepersongenerator.com/Face/male/male20161086287283478.jpg",
+    createdAt: "2h",
+  },
+  {
+    id: 3,
+    username: "paryakhadivand",
+    userImage:
+      "https://www.fakepersongenerator.com/Face/female/female1022713364411.jpg",
+    threadText:
+      "Throwback to coding in a cafe in Belgium! Can we talk about how Europe is amazing and that I should DEFINITELY move there because I’ll do it 😩",
+    likes: 122,
+    replies: 15,
+    userWhoReplies:
+      "https://www.fakepersongenerator.com/Face/male/male20161086287283478.jpg",
+    createdAt: "3h",
+  },
+  {
+    id: 4,
+    username: "diyarkarimzadeh",
+    userImage:
+      "https://www.fakepersongenerator.com/Face/female/female20161025235219666.jpg",
+    threadText:
+      "I still can't believe I worked at a company that told me they had “three years runway” when I joined. I was a part of the layoffs they did three months after that. 🙃",
+    likes: 122,
+    replies: 15,
+    userWhoReplies:
+      "https://www.fakepersongenerator.com/Face/male/male20161086287283478.jpg",
+    createdAt: "3h",
+  },
+  {
+    id: 5,
+    username: "diyarkarimzadeh",
+    userImage:
+      "https://www.fakepersongenerator.com/Face/female/female20161025235219666.jpg",
+    threadText:
+      "Btw I’ve been learning three.js for fun and just so y’all know, my personal website is about to be absolute 🔥",
+    likes: 122,
+    replies: 15,
+    userWhoReplies:
+      "https://www.fakepersongenerator.com/Face/male/male20161086287283478.jpg",
+    createdAt: "3h",
+  },
+];
